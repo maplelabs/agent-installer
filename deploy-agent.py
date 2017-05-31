2eaf7b024567ed4e2a001c5272c41788dc57724b
mahanth <mahanth.gn@maplelabs.com>
2017-05-31 17:48:18 +0530

    updated

--- deploy-agent.py
+++ deploy-agent.py
@@ -318,7 +318,7 @@ def install_configurator(host, port):
         # run_cmd("kill $(ps -face | grep -v grep | grep 'api_server' | awk '{print $2}')", shell=True, ignore_err=True)
         # cmd2 = "cd " + CONFIGURATOR_DIR
         # cmd2 += " && python api_server.py -i {0} -p {1} > /dev/null 2>&1 & disown".format(host, port)
-        cmd2 = "sudo nohup python {0}/api_server.py -i {1} -p {2} &".format(CONFIGURATOR_DIR, host, port)
+        cmd2 = "sudo nohup python {0}/api_server.py -i {1} -p {2} </dev/null >/dev/null 2>&1 &".format(CONFIGURATOR_DIR, host, port)
         print cmd2
         run_call(cmd2, shell=True)
 
