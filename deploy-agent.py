22dfebde159a99aedc64421fe0cb8dd88d08f4dd
mahanth <mahanth.gn@maplelabs.com>
2017-06-01 18:43:38 +0530

    changed command

--- deploy-agent.py
+++ deploy-agent.py
@@ -327,11 +327,13 @@ def install_configurator(host, port):
         # sleep(5)
         if platform.dist()[0].lower() == "ubuntu":
             cmd2 = "sudo nohup python {0}/api_server.py -i {1} -p {2} &".format(CONFIGURATOR_DIR, host, port)
+            run_call(cmd2, shell=True)
+            sleep(5)
         elif platform.dist()[0].lower() == "centos":
             cmd2 = "sudo nohup python {0}/api_server.py -i {1} -p {2} &> /dev/null &".format(CONFIGURATOR_DIR, host,
                                                                                              port)
-        run_call(cmd2, shell=True)
-        sleep(5)
+            run_call(cmd2, shell=True)
+            sleep(5)
 
 
 def create_configurator_service():
