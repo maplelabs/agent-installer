177328c3a2c35b81dd0d9a593a74c5772041de1a
upendrasahu <upendra.sahu@maplelabs.com>
2017-06-21 15:39:11 +0530

    Bug fix

--- install-agent.py
+++ install-agent.py
@@ -594,6 +594,7 @@ def install(collectd=True, fluentd=True, configurator=True, configurator_host="0
     obj.stop_configurator_process()
     obj.install_dev_tools()
     obj.install_pip()
+    obj.install_python_packages()
 
     if configurator:
         print "started installing configurator ..."
@@ -603,7 +604,6 @@ def install(collectd=True, fluentd=True, configurator=True, configurator_host="0
 
     if collectd:
         print "Started installing collectd ..."
-        obj.install_python_packages()
         obj.setup_collectd()
         obj.add_collectd_plugins()
         obj.create_collectd_service()
