9fa162879231a12a23047102682e126619bc9a19
Vivek Akabote <42860633+vivekatmaplelabs@users.noreply.github.com>
2018-12-17 18:17:36 +0530

    Update install-agent.py

--- install-agent.py
+++ install-agent.py
@@ -191,9 +191,9 @@ class DeployAgent:
         #self.pip = 'pip'
         self.python = 'python'
         self.pip = self.python + " -m pip"
-        #if os.path.isfile("/usr/bin/python"):
-        #    self.python = "/usr/bin/python"
-        #    self.pip = self.python + " -m pip"
+        if os.path.isfile("/usr/bin/python"):
+            self.python = "/usr/bin/python"
+            self.pip = self.python + " -m pip"
             
     def _run_cmd(self, cmd, shell, ignore_err=False, print_output=False):
         """
